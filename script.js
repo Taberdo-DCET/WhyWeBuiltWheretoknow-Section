8ec48db3bcb35ba3e355d77819b56c45a770ee5a
Taberdo-DCET <taberdoraphael189@gmail.com>
2026-02-20 16:19:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
         contents.forEach(el => el.style.opacity = '1');
         maps.forEach(el => el.style.opacity = '1');
     }, 100);
+
+    const color1 = document.getElementById('color1');
+    const hex1 = document.getElementById('hex1');
+    const v1Pins = document.querySelectorAll('.v1-pin');
+
+    color1.addEventListener('input', (e) => {
+        const newColor = e.target.value;
+        hex1.textContent = newColor.toUpperCase();
+        v1Pins.forEach(pin => pin.style.color = newColor);
+    });
+
+    const color2 = document.getElementById('color2');
+    const hex2 = document.getElementById('hex2');
+    const v2Pins = document.querySelectorAll('.v2-pin');
+
+    color2.addEventListener('input', (e) => {
+        const newColor = e.target.value;
+        hex2.textContent = newColor.toUpperCase();
+        v2Pins.forEach(pin => pin.style.color = newColor);
+    });
 });
